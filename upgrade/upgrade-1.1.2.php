6d7b51d3eab54d61f7a03c806d678b0cfa3a783c
Sauli Maijala <sauli@vilkas.fi>
2023-04-25 10:51:03 +0300

    update the upgrade script

--- upgrade/upgrade-1.1.2.php
+++ upgrade/upgrade-1.1.2.php
@@ -9,6 +9,8 @@ if (!defined("_PS_VERSION_")) {
  */
 function upgrade_module_1_1_2(Vg_postnord $module): bool
 {
+    $success = true;
+
     // convert additional_service_codes from JSON string to array in carrier config
     $carrier_config = $module->getCarrierConfigurations();
     foreach ($carrier_config as &$config) {
@@ -18,7 +20,17 @@ function upgrade_module_1_1_2(Vg_postnord $module): bool
         }
     }
 
-    return Configuration::updateValue('VG_POSTNORD_CARRIER_SETTINGS', json_encode($carrier_config))
+    // move shop_party_id from address settings to basic settings for visibility
+    $shop_address = json_decode(Configuration::get("VG_POSTNORD_SHOP_ADDRESS", true), true);
+    if (array_key_exists("shop_party_id", $shop_address)) {
+        $shop_party_id = $shop_address["shop_party_id"];
+        unset($shop_address["shop_party_id"]);
+        $success &= Configuration::updateValue("VG_POSTNORD_SHOP_ADDRESS", json_encode($shop_address));
+        $success &= Configuration::updateValue("VG_POSTNORD_PARTY_ID", $shop_party_id);
+    }
+
+    return $success
+        && Configuration::updateValue("VG_POSTNORD_CARRIER_SETTINGS", json_encode($carrier_config))
         && $module->unregisterHook("header")
         && $module->registerHook("displayHeader");
 }
