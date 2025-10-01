786d6e594d44ab18a369657798daa48111df48b3
Sauli Maijala <sauli@vilkas.fi>
2025-10-01 21:55:36 +0300

    move function definition outside loop

--- views/js/config.js
+++ views/js/config.js
@@ -1,30 +1,18 @@
 $(document).ready(() => {
   const countryValidCombinations = validCombinations;
 
-  // use checkbox to fill in hidden text field
-  $('.additional_service_codes').each(function () {
-    const hiddenInput = this;
-
-    // Find service_code_consigneecountry to populate correct checkboxes
-    const serviceCodeField = $(
-      `#${hiddenInput.id.replace(
-        'additional_service_codes',
-        'service_code_consigneecountry'
-      )}`
-    );
-
-    /**
-     * Add checkbox in place of hiddenInput field with value from countryValidCombinations
-     * filtered with serviceCodeField
-     * @param {HTMLInputElement} hiddenInput
-     * @param {HTMLSelectElement} serviceCodeField
-     * @param {Object} countryValidCombinations
-     */
-    function addCheckbox(
+  /**
+   * Add checkbox in place of hiddenInput field with value from countryValidCombinations
+   * filtered with serviceCodeField
+   * @param {HTMLInputElement} hiddenInput
+   * @param {HTMLSelectElement} serviceCodeField
+   * @param {Object} countryValidCombinations
+   */
+  function addCheckbox(
       hiddenInput,
       serviceCodeField,
       countryValidCombinations
-    ) {
+  ) {
       const [code, country] = serviceCodeField.val().split('_');
 
       // filter additional service codes by service code and country
@@ -37,21 +25,33 @@ $(document).ready(() => {
       // Sort then loop through the filteredCombination to create checkboxes
       filteredCombinations.forEach(function (combination) {
           checkboxesContent += `
-            <div class="checkbox">
-              <label for='${hiddenInput.id}_${combination.adnlServiceCode}'>
-              <input type="checkbox" name="checkbox" ${hiddenInput.value.includes(combination.adnlServiceCode) ? 'checked' : ''}
-                     id="${hiddenInput.id}_${combination.adnlServiceCode}"
-                     value="${combination.adnlServiceCode}">
-                ${combination.adnlServiceCode} - ${combination.adnlServiceName}
-              </label>
-            </div>
-          `;
-        });
+          <div class="checkbox">
+            <label for='${hiddenInput.id}_${combination.adnlServiceCode}'>
+            <input type="checkbox" name="checkbox" ${hiddenInput.value.includes(combination.adnlServiceCode) ? 'checked' : ''}
+                   id="${hiddenInput.id}_${combination.adnlServiceCode}"
+                   value="${combination.adnlServiceCode}">
+              ${combination.adnlServiceCode} - ${combination.adnlServiceName}
+            </label>
+          </div>
+        `;
+      });
 
       // Add checkboxes then click to update/clear hiddenInputField
       $(hiddenInput).parent().find('.checkboxes').html(checkboxesContent);
       $(hiddenInput).parent().find('.checkboxes').click();
-    }
+  }
+
+  // use checkbox to fill in hidden text field
+  $('.additional_service_codes').each(function () {
+    const hiddenInput = this;
+
+    // Find service_code_consigneecountry to populate correct checkboxes
+    const serviceCodeField = $(
+      `#${hiddenInput.id.replace(
+        'additional_service_codes',
+        'service_code_consigneecountry'
+      )}`
+    );
 
     $(hiddenInput).parent().prepend('<div class="checkboxes"></div>');
     addCheckbox(hiddenInput, serviceCodeField, countryValidCombinations);
