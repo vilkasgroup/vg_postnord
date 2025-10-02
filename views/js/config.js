28b351c3b40fe470b5d667f55c02fa29bcdb7866
Sauli Maijala <sauli@vilkas.fi>
2025-10-02 14:38:39 +0300

    a single click handler for service code change

--- views/js/config.js
+++ views/js/config.js
@@ -13,7 +13,7 @@ $(document).ready(() => {
       serviceCodeField,
       countryValidCombinations
   ) {
-      const [code, country] = serviceCodeField.val().split('_');
+      const [code, country] = $(serviceCodeField).val().split('_');
 
       // filter additional service codes by service code and country
       const filteredCombinations = countryValidCombinations.filter(
@@ -54,11 +54,6 @@ $(document).ready(() => {
 
     $(hiddenInput).parent().prepend('<div class="checkboxes"></div>');
     addCheckbox(hiddenInput, serviceCodeField, countryValidCombinations);
-
-    // Update checkbox after service code changed
-    $(serviceCodeField).change(() => {
-      addCheckbox(hiddenInput, serviceCodeField, countryValidCombinations);
-    });
   });
 
   // update the hidden input field when checkboxes are clicked
@@ -73,5 +68,19 @@ $(document).ready(() => {
       }
     });
     hiddenInput.value = JSON.stringify(inputValue);
+  })
+
+  // update checkboxes when carrier service code is changed
+  $(document.body).on('change', 'select[id$="_service_code_consigneecountry"]', function () {
+    const serviceCodeField = this;
+
+    // find the corresponding hidden input field
+    const hiddenInputId = '#' + serviceCodeField.id.replace(
+        '_service_code_consigneecountry',
+        '_additional_service_codes'
+    );
+    const hiddenInput = $(hiddenInputId)[0];
+
+    addCheckbox(hiddenInput, serviceCodeField, countryValidCombinations);
   })
 });
